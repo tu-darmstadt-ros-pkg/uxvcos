0bb0dcb38b87e501074c6d761e2cd4680a6f93ee
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2013-08-01 18:16:38 +0200

    setup: only mount the log partition if it exists

--- etc/setup.d/mount_log.sh
+++ etc/setup.d/mount_log.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-log=`readlink $UXVCOS_LOG || echo "$UXVCOS_LOG"`
-if mountpoint -q $log; then return 0; fi
-mount $log
+if [ -s /dev/disk/by-label/log ]; then
+  log=`readlink $UXVCOS_LOG || echo "$UXVCOS_LOG"`
+  if mountpoint -q $log; then return 0; fi
+  mount /dev/disk/by-label/log $log -o noatime
+fi
